31081d540422fa962e2d96773a2a6769a1203cfb
Khouloud Ben Salem <khouloud.bensalem@ensi-uma.tn>
2024-11-12 16:59:10 +0100

    first commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  constructor(private messageService: MessageService) {}
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES); // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+}
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [NgFor, FormsModule, RouterLink],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css',
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+}
